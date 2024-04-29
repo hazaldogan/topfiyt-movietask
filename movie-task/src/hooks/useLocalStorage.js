21f72ce0d18f96218942d42bba277d4d50ceec4f
Hazal Taştan <hazl.dogann@gmail.com>
2024-04-29 15:55:47 +0300

    useLocalStorage add

---
+++ movie-task/src/hooks/useLocalStorage.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = localStorage.getItem(key);
+    if (item) {
+      try {
+        return JSON.parse(item);
+      } catch (e) {
+        return item;
+      }
+    } else return initialValue;
+  });
+  const setValue = (value) => {
+    setStoredValue(value);
+    if (
+      typeof value === "string" ||
+      typeof value === "number" ||
+      typeof value === "boolean"
+    ) {
+      localStorage.setItem(key, value);
+    } else {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  };
+  return [storedValue, setValue];
+};
