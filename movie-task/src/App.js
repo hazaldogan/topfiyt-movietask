1d7b75ae10492dd864de4e681ff00c7effaa383a
Hazal Taştan <hazl.dogann@gmail.com>
2024-04-29 15:55:14 +0300

    create react app

---
+++ movie-task/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+import Footer from "./layout/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
