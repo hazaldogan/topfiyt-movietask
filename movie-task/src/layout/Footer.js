0694cbfa89d3943b00c1299a2928e8ea663d36f3
Hazal Taştan <hazl.dogann@gmail.com>
2024-04-29 16:58:07 +0300

    Footer compllited

--- movie-task/src/layout/Footer.js
+++ movie-task/src/layout/Footer.js
@@ -2,13 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-evenly items-center">
-      <Link to="/">
-        <img
-          className="h-20"
-          src="https://www.topfiyt.com/template/assets/images/logo.png"
-        />
-      </Link>
+    <footer className="flex justify-around items-center p-10 max-sm:flex-col max-sm:gap-3">
+      <div>
+        <Link
+          to="/"
+          className="no-underline flex justify-center items-center gap-3"
+        >
+          <img
+            className="h-20"
+            src="https://www.topfiyt.com/template/assets/images/logo.png"
+          />
+          <h2 className="text-3xl font-bold text-teal-400 no-underline">
+            TopFiyt
+          </h2>
+        </Link>
+      </div>
+
       <div className="flex flex-col gap-4 max-sm:gap-2">
         <h5 className="text-slate-800 text-base font-bold  max-sm:text-sm">
           Get in Touch
