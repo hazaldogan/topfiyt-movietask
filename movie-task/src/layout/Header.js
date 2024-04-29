58e763ca199a290c57f8999210ff88448fa8806d
Hazal Taştan <hazl.dogann@gmail.com>
2024-04-29 16:57:59 +0300

    Header complited

--- movie-task/src/layout/Header.js
+++ movie-task/src/layout/Header.js
@@ -1,18 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    <header className="flex p-5 justify-between items-center">
-      <Link to="/">
-        <img
-          className="h-20"
-          src="https://www.topfiyt.com/template/assets/images/logo.png"
-        />
-      </Link>
-      <nav className="flex gap-10 justify-start items-start text-gray-600 font-bold">
-        <NavLink to="/">Home Page</NavLink>
-        <NavLink to="/fav-movies">Favorite Movies</NavLink>
+    <header className="flex p-2 justify-between items-center">
+      <div>
+        <Link
+          to="/"
+          className="no-underline flex justify-center items-center gap-3"
+        >
+          <img
+            className="h-20"
+            src="https://www.topfiyt.com/template/assets/images/logo.png"
+          />
+          <h2 className="text-3xl font-bold text-teal-400 no-underline">
+            TopFiyt
+          </h2>
+        </Link>
+      </div>
+      <nav className="flex gap-10 justify-start items-start font-bold max-sm:hidden">
+        <NavLink to="/" className="no-underline  text-gray-600">
+          Home Page
+        </NavLink>
+        <NavLink to="/fav-movies" className="no-underline  text-gray-600">
+          Favorite Movies
+        </NavLink>
       </nav>
+      <div className="hidden max-sm:flex gap-4">
+        <FontAwesomeIcon icon={faHouse} size="lg" />
+        <FontAwesomeIcon icon={faHeart} size="lg" />
+      </div>
     </header>
   );
 }
