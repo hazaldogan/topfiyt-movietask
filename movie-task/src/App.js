95455a5f01d55201abb7b0f7187ecc1fcce917db
Hazal Taştan <hazl.dogann@gmail.com>
2024-04-30 14:14:05 +0300

    refactor

--- movie-task/src/App.js
+++ movie-task/src/App.js
@@ -5,10 +5,12 @@ import Main from "./layout/Main";
 import Footer from "./layout/Footer";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getPopularMovies } from "./store/actions/movieActions";
-import { getLatestMovies } from "./store/actions/movieActions";
-import { getTopRatedMovies } from "./store/actions/movieActions";
-import { getUpComingMovies } from "./store/actions/movieActions";
+import {
+  getPopularMovies,
+  getLatestMovies,
+  getTopRatedMovies,
+  getUpComingMovies,
+} from "./store/actions/movieActions";
 
 function App() {
   const dispatch = useDispatch();
@@ -21,7 +23,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App bg-gray-800">
       <Header />
       <Main />
       <Footer />
